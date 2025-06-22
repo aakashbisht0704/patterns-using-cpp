43398fb6f31d0547a2e8e05c2831292e239a8b12
aakashbisht0704 <aakashbisht1204@gmail.com>
2025-06-22 14:09:41 +0530

    Inverted Hollow Half Pyramid implemented in CPP

---
+++ inverted-hollow-half-pyramid.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n = 5;
+    for(int i=0; i<n; i++){
+        for(int j=n-1; j>=i; j--){
+            if(i==0 || j==i || j==n-1){
+                cout << " *";
+            }
+            else{
+                cout << "  ";
+            }
+        }
+        cout << endl;
+    }
+}

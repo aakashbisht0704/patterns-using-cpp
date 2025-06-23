0d9fc287879f24ebde6a084b1b675eb36df3102e
aakashbisht0704 <aakashbisht1204@gmail.com>
2025-06-23 14:10:16 +0530

    Diamond Pattern implemented in CPP

---
+++ diamond-pattern.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+void fullPyramid(int n){
+    for(int i=0; i<n; i++){
+        //spaces
+        for(int j=0; j<(n-i-1); j++) cout<< " ";
+
+        //stars
+        for(int j=0; j<(i+1); j++) cout << "* ";
+        cout << endl;
+    }
+}
+
+void invertedFullPyramid(int n){
+    for(int i=0; i<n; i++){
+        //spaces
+        for(int j=0; j<i; j++) cout << " ";
+
+        //stars
+        for(int j=0; j<(n-i); j++) cout << "* ";
+        cout << endl;
+    }
+}
+
+
+int main(){
+    int n = 10;
+    fullPyramid(n);
+    invertedFullPyramid(n);
+}

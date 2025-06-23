8499d2e1512cd80ab1565103161fedf2c20655f6
aakashbisht0704 <aakashbisht1204@gmail.com>
2025-06-23 13:25:09 +0530

    Full Pyramid implemented in CPP code updated

--- full-pyramid.cpp
+++ full-pyramid.cpp
@@ -4,12 +4,12 @@ using namespace std;
 
 void fullPyramid(int n){
     for(int i=0; i<n;i++){
-        for(int j=0; j<(n-i-1); j++){
+        for(int j=0; j<(n-i-1); j++)
             cout << " ";
-        }
-        for(int j=0; j<i+1; j++){
+
+        for(int j=0; j<i+1; j++)
             cout << "* ";
-        }
+
         cout << endl;
     }
 }
